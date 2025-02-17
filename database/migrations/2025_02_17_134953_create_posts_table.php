373b78457fb883b111c3852d2cb53f62ae2df4da
Coderyounes <younesanas2021@gmail.com>
2025-02-17 22:45:01 +0100

    Generate User & Posts Migrations

--- database/migrations/2025_02_17_134953_create_posts_table.php
+++ database/migrations/2025_02_17_134953_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->nullable(false);
+            $table->text('content')->nullable(false);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
