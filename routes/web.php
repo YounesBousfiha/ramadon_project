84adba4b3e9753c7157880ff28c41d8d650f737b
Coderyounes <younesanas2021@gmail.com>
2025-02-17 22:45:10 +0100

    define Recettes Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecetteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Recette Routes
+Route::get('/recettes', [RecetteController::class, 'index']);
+Route::post('/recettes/create', [RecetteController::class, 'store']);
+Route::get('/recette/{id}', [RecetteController::class, 'show']);
+Route::post('/recette/update/{id}', [RecetteController::class, 'update']);
+Route::post('/recette/delete/{id}', [RecetteController::class, 'destroy']);
+
+// Post Routes
+
+// Comments Controllers
+
