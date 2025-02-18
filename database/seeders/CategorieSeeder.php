f185cfa558710908479adc7697fb50dfc2c27741
Coderyounes <younesanas2021@gmail.com>
2025-02-18 14:37:18 +0100

    Create a categorie Seeder

---
+++ database/seeders/CategorieSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categorie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = ['salade', 'entree', 'desert'];
+        foreach ($data as $entry ) {
+            Categorie::create(['categorie_nom' => $entry]);
+        }
+    }
+}
