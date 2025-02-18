00c3ff035e0703beba9098147a5d5a993e83c306
Coderyounes <younesanas2021@gmail.com>
2025-02-18 14:35:48 +0100

    setup relation Comment Model with recette & post Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+
+    public function recette() {
+        return $this->belongsTo(Recette::class);
+    }
+
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
 }
