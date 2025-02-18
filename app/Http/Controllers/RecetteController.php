c033add22b1e4052fbdc38ce390d0fee2d20553c
Coderyounes <younesanas2021@gmail.com>
2025-02-18 21:48:32 +0100

    Setup Recette Controllers

--- app/Http/Controllers/RecetteController.php
+++ app/Http/Controllers/RecetteController.php
@@ -12,7 +12,8 @@ class RecetteController extends Controller
      */
     public function index()
     {
-
+        $recettes = Recette::all();
+        return view('recette.index', compact('recettes'));
     }
 
 
@@ -21,30 +22,51 @@ class RecetteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'prenom' => 'required|string|max:255',
+            'nom' => 'required|string|max:255',
+            'ingredient' => 'required|string|max:255',
+            'instruction' => 'required|string|max:2000',
+            'categorie_id' => 'required|number'
+        ]);
+
+        Recette::create($request->all());
+        return redirect()->route('recette.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Recette $recette)
+    public function show($id)
     {
-        //
+        $recette = Recette::findOrfail($id);
+        return view('recette.show', compact('recette'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Recette $recette)
+    public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'ingredient' => 'required|string|max:255',
+            'instruction' => 'required|string|max:2000'
+        ]);
+
+        $recette = Recette::findOrfail($id);
+        $recette->update($request->only(['ingredient', 'instruction']));
+
+        return redirect()->route('recette.index');
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Recette $recette)
+    public function destroy($id)
     {
-        //
+        $recette = Recette::findOrfail($id);
+        $recette->delete($id);
+        return redirect()->route('recette.index');
     }
 }
