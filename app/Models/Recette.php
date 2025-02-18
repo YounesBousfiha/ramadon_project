7d7274ba42489f262668bd4f80a01da8844628de
Coderyounes <younesanas2021@gmail.com>
2025-02-18 14:36:28 +0100

    setup relation in Recette Model with categorie & comment

--- app/Models/Recette.php
+++ app/Models/Recette.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Recette extends Model
 {
     use HasFactory;
+
+    public function categorie() {
+        return $this->belongsTo(Categorie::class);
+    }
+
+    public function comment() {
+        return $this->hasMany(Comment::class);
+    }
 }
