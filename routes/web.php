f527a261b7a45287dbf18dd494724a290e13c864
Coderyounes <younesanas2021@gmail.com>
2025-02-19 23:32:07 +0100

    add name to some routes

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,16 @@ Route::post('/recette/delete/{id}', [RecetteController::class, 'destroy']);
 // Post Routes
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::post('/posts/create', [PostController::class, 'store']);
-Route::get('/post/{id}', [PostController::class, 'show']);
+Route::get('/post/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/post/update/{id}', [PostController::class, 'update'])->name('posts.update');
 Route::post('/post/delete/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 // Comments Controllers
 Route::get('/comments', [CommentController::class, 'index']);
-Route::post('/comments/create', [CommentController::class, 'store']);
+Route::post('/comments/create', [CommentController::class, 'store'])->name('comment.create');
 Route::get('/comment/{id}', [CommentController::class, 'show']);
 Route::post('/comment/update/{id}', [CommentController::class, 'update']);
-Route::post('/comment/delete/{id}', [CommentController::class, 'destroy']);
+Route::post('/comment/delete/{id}', [CommentController::class, 'destroy'])->name('comment.delete');
 
 // Categorie Controllers
 Route::get('/categories', [CategorieController::class, 'index']);
